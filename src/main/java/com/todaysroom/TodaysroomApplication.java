bdbc5c4726bd78616f2383888c81012faf8c29c8
seoky <sseok1979@naver.com>
2023-03-22 14:26:55 +0900

    project 생성

---
+++ src/main/java/com/todaysroom/TodaysroomApplication.java
@@ -0,0 +1,13 @@
+package com.todaysroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysroomApplication.class, args);
+	}
+
+}
