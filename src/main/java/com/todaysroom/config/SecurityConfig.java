82b3773567ff02db753e7243f2af7a67df0f5d3f
seoky <sseok1979@naver.com>
2023-04-18 18:27:18 +0900

    security cors ãsetting

--- src/main/java/com/todaysroom/config/SecurityConfig.java
+++ src/main/java/com/todaysroom/config/SecurityConfig.java
@@ -6,14 +6,25 @@ import com.todaysroom.user.jwt.JwtSecurityConfig;
 import com.todaysroom.user.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -28,18 +39,22 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .cors()
+                .and().csrf().disable()
+
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .accessDeniedHandler(jwtAccessDeniedHandler)
 
-                .and()
-                .headers()
-                .frameOptions()
-                .sameOrigin()
+//                .and()
+//                .headers()
+//                .frameOptions()
+//                .sameOrigin()
 
                 .and()
                 .sessionManagement()
@@ -47,8 +62,9 @@ public class SecurityConfig {
 
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/authenticate").permitAll()
-                .requestMatchers("/api/signup").permitAll()
+                //.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .requestMatchers("/users/login").permitAll()
+                .requestMatchers("/users/signup").permitAll()
 
                 .anyRequest().authenticated()
                 .and()
@@ -56,6 +72,8 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer(){
