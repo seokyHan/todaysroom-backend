9365e5db92b250d54e8726414a214903774fb312
seokyHan <sseok1979@naver.com>
2023-10-26 11:12:14 +0900

    파일 삭제 스케줄링 추가

--- src/main/java/com/todaysroom/common/file/service/FileService.java
+++ src/main/java/com/todaysroom/common/file/service/FileService.java
@@ -12,10 +12,12 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -25,6 +27,7 @@ import java.nio.file.StandardCopyOption;
 import java.text.Normalizer;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -138,5 +141,36 @@ public class FileService {
         return fileList;
     }
 
+    // @Scheduled(cron = "0 * * * * *") 1 분마다 실행
+    @Scheduled(cron = "0 0 18 * * *") // 매일 오후 18시에 실행
+    public void fileDeleteScheduled() {
+        List<UserFiles> fileList = userFilesRepository.findAll();
+
+        try{
+            // 해당 경로 하위 폴더 탐색 후 디렉토리가 아닌 일반파일인지 체크
+            List<Path> filePath = Files.walk(imageDirectoryPath)
+                    .filter(Files::isRegularFile)
+                    .collect(Collectors.toList());
+
+            for(Path path : filePath){
+                String fileName = path.getFileName().toString();
+                if(!isFilePresent(fileName, fileList)){
+                    Files.delete(path);
+                }
+            }
+        }catch (IOException e){
+            log.info(e.getMessage());
+        }
+    }
+
+    private boolean isFilePresent(String fileName, List<UserFiles> fileList){
+        for(UserFiles userFile : fileList){
+            if(userFile.getFileName().equals(fileName)){
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 }
