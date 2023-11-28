acf5b29a379f8a0d0980e6889c511aaf2f80581f
seokyHan <sseok1979@naver.com>
2023-11-28 18:23:34 +0900

    fix: redis cofig 수정

--- src/main/java/com/todaysroom/global/config/redis/RedisConfig.java
+++ src/main/java/com/todaysroom/global/config/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.todaysroom.global.config;
+package com.todaysroom.global.config.redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
