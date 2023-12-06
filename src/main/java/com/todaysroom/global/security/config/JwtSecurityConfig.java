b3df5bb126f6f12a14c0e3439ee43f0fdf2e2c54
seokyHan <sseok1979@naver.com>
2023-12-06 17:43:01 +0900

    add: jwt config

--- src/main/java/com/todaysroom/global/security/config/JwtSecurityConfig.java
+++ src/main/java/com/todaysroom/global/security/config/JwtSecurityConfig.java
@@ -1,5 +1,7 @@
-package com.todaysroom.user.jwt;
+package com.todaysroom.global.security.config;
 
+import com.todaysroom.global.security.filter.JwtFilter;
+import com.todaysroom.global.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
