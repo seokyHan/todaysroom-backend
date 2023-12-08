6d705ae57cf857de3d7664bfc5e487bcca04e95f
seokyHan <sseok1979@naver.com>
2023-12-08 18:14:13 +0900

    add: restTemplate config 추가

--- src/main/java/com/todaysroom/global/common/config/restTemplate/RestTemplateConfig.java
+++ src/main/java/com/todaysroom/global/common/config/restTemplate/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.todaysroom.global.config.restTemplate;
+package com.todaysroom.global.common.config.restTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
