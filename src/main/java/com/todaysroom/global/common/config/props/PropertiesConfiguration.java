634880f0453f6bb12699d8b7e80015dc5cfdbc27
seokyHan <sseok1979@naver.com>
2023-12-13 18:21:40 +0900

    add: propertiesConfiguration 추가

---
+++ src/main/java/com/todaysroom/global/common/config/props/PropertiesConfiguration.java
@@ -0,0 +1,18 @@
+package com.todaysroom.global.common.config.props;
+
+import com.todaysroom.global.common.config.redis.props.RedisProperties;
+import com.todaysroom.global.security.props.JWTProperties;
+import com.todaysroom.map.props.HouseDealProperties;
+import com.todaysroom.map.props.KaKaoProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(value = {
+        JWTProperties.class,
+        HouseDealProperties.class,
+        KaKaoProperties.class,
+        RedisProperties.class
+})
+public class PropertiesConfiguration {
+}
