b4aa7e874b3f7710d273aac144fb39f468f25be3
seokyHan <sseok1979@naver.com>
2023-12-15 18:18:06 +0900

    fix: refactoring config

--- src/main/java/com/todaysroom/global/security/config/WebMvcConfig.java
+++ src/main/java/com/todaysroom/global/security/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.todaysroom.global.security;
+package com.todaysroom.global.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
