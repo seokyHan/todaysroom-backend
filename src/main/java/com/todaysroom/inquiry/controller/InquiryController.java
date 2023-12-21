a5920c0713e07a8867b39aedf1d6781891df5e71
seokyHan <sseok1979@naver.com>
2023-12-21 17:51:05 +0900

    fix: refactoring InquiryController

--- src/main/java/com/todaysroom/inquiry/controller/InquiryController.java
+++ src/main/java/com/todaysroom/inquiry/controller/InquiryController.java
@@ -2,19 +2,13 @@ package com.todaysroom.inquiry.controller;
 
 import com.todaysroom.inquiry.dto.InquiryAnswerDto;
 import com.todaysroom.inquiry.dto.InquiryUpdateDto;
-import com.todaysroom.inquiry.exception.NoInquiryAnswerIdException;
-import com.todaysroom.inquiry.exception.NoInquiryIdException;
-import com.todaysroom.user.exception.NoUserException;
 import com.todaysroom.inquiry.dto.InquiryRequestDto;
 import com.todaysroom.inquiry.dto.InquiryResponseDto;
 import com.todaysroom.inquiry.service.InquiryService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,12 +24,12 @@ public class InquiryController {
     private final InquiryService inquiryService;
 
     @GetMapping
-    public ResponseEntity<List<InquiryResponseDto>> getInquiriesByUserId(@RequestParam("userId") Long userId) throws NoUserException {
+    public ResponseEntity<List<InquiryResponseDto>> getInquiriesByUserId(@RequestParam("userId") Long userId) {
         return ResponseEntity.ok(inquiryService.getInquiriesByUserId(userId));
     }
 
     @GetMapping("/detail/{id}")
-    public ResponseEntity<InquiryResponseDto> getInquiriesById(@PathVariable("id") Long id) throws NoInquiryIdException {
+    public ResponseEntity<InquiryResponseDto> getInquiriesById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(inquiryService.getInquiresById(id));
     }
 
@@ -46,7 +40,7 @@ public class InquiryController {
 
     @PostMapping("/create")
     public ResponseEntity<String> create(@RequestPart(value = "inquiryRequestDto") InquiryRequestDto inquiryRequestDto,
-                                       @RequestPart(value = "fileList", required = false) List<MultipartFile> fileList) throws NoUserException {
+                                       @RequestPart(value = "fileList", required = false) List<MultipartFile> fileList) {
 
         InquiryResponseDto postResponseDto = inquiryService.save(inquiryRequestDto, fileList);
         return ResponseEntity.created(URI.create("/" + postResponseDto.id())).build();
@@ -55,25 +49,25 @@ public class InquiryController {
 
     @PutMapping("/update")
     public ResponseEntity<String> modify(@RequestPart(value = "inquiryUpdateDto") InquiryUpdateDto inquiryUpdateDto,
-                                         @RequestPart(value = "fileList", required = false) List<MultipartFile> fileList) throws NoInquiryIdException {
+                                         @RequestPart(value = "fileList", required = false) List<MultipartFile> fileList) {
         return new ResponseEntity<>("id :" + inquiryService.update(inquiryUpdateDto, fileList) + " 수정완료", HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> remove(@PathVariable("id") Long id) throws NoInquiryIdException {
+    public ResponseEntity<String> remove(@PathVariable("id") Long id) {
         inquiryService.delete(id);
 
         return new ResponseEntity<>("id :" + id + " 삭제 완료", HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/answer")
-    public ResponseEntity<String> inquiryAnswerSave(@RequestBody InquiryAnswerDto inquiryAnswerDto) throws NoInquiryIdException{
+    public ResponseEntity<String> inquiryAnswerSave(@RequestBody InquiryAnswerDto inquiryAnswerDto){
 
         return ResponseEntity.ok("Inquiry Answer ID : " + inquiryService.answerSave(inquiryAnswerDto) + " 답변 저장 완료");
     }
 
     @DeleteMapping("/answer")
-    public ResponseEntity<String> inquiryAnswerDelete(@RequestParam("id") Long id, @RequestParam("inquiryId") Long inquiryId) throws NoInquiryAnswerIdException, NoInquiryIdException{
+    public ResponseEntity<String> inquiryAnswerDelete(@RequestParam("id") Long id, @RequestParam("inquiryId") Long inquiryId) {
 
         return ResponseEntity.ok("Inquiry Answer ID : " + inquiryService.answerDelete(id, inquiryId) + " 답변 삭제 완료");
     }
