b316dadfca9f483f2175080edf7834f95d0ff336
seokyHan <sseok1979@naver.com>
2023-12-27 13:08:52 +0900

    fix: refactoring inquiry service

--- src/main/java/com/todaysroom/inquiry/service/InquiryService.java
+++ src/main/java/com/todaysroom/inquiry/service/InquiryService.java
@@ -4,12 +4,10 @@ import com.todaysroom.file.dto.UserFileRequestDto;
 import com.todaysroom.file.entity.FilesLocation;
 import com.todaysroom.file.entity.UserFiles;
 import com.todaysroom.file.service.FileService;
+import com.todaysroom.global.exception.CustomException;
 import com.todaysroom.inquiry.dto.InquiryAnswerDto;
 import com.todaysroom.inquiry.dto.InquiryUpdateDto;
 import com.todaysroom.inquiry.entity.InquiryAnswer;
-import com.todaysroom.inquiry.exception.NoInquiryAnswerIdException;
-import com.todaysroom.inquiry.exception.NoInquiryIdException;
-import com.todaysroom.user.exception.NoUserException;
 import com.todaysroom.inquiry.dto.InquiryRequestDto;
 import com.todaysroom.inquiry.dto.InquiryResponseDto;
 import com.todaysroom.inquiry.entity.Inquiry;
@@ -25,6 +23,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.todaysroom.global.exception.code.AuthResponseCode.USER_NOT_FOUND;
+import static com.todaysroom.global.exception.code.CommonResponseCode.REQUIRED_ERROR;
 
 @Service
 @Slf4j
@@ -44,16 +45,16 @@ public class InquiryService {
     }
 
     @Transactional
-    public List<InquiryResponseDto> getInquiriesByUserId(Long userId) throws NoUserException {
-        UserEntity userEntity = userRepository.findById(userId).orElseThrow(NoUserException::new);
+    public List<InquiryResponseDto> getInquiriesByUserId(Long userId) {
+        UserEntity userEntity = userRepository.findById(userId).orElseThrow(() -> new CustomException(USER_NOT_FOUND, "해당 User를 찾을 수 없습니다."));
         List<Inquiry> inquiries = inquiryRepository.findByUserEntityId(userEntity.getId());
 
         return inquiries.stream().map(InquiryResponseDto::from).collect(Collectors.toList());
     }
 
     @Transactional
-    public InquiryResponseDto getInquiresById(Long id) throws NoInquiryIdException{
-        Inquiry inquiry = inquiryRepository.findById(id).orElseThrow(NoInquiryIdException::new);
+    public InquiryResponseDto getInquiresById(Long id) {
+        Inquiry inquiry = inquiryRepository.findById(id).orElseThrow(() -> new CustomException(REQUIRED_ERROR, "문의 하신 게시글이 없습니다."));
         FilesLocation filesLocation = fileService.findByFileLocationId(1L);
         List<UserFiles> fileList = fileService.findByPostIdAndFileLocationId(inquiry.getId(), filesLocation.getId());
 
@@ -66,8 +67,8 @@ public class InquiryService {
 
 
     @Transactional
-    public InquiryResponseDto save(InquiryRequestDto inquiryRequestDto, List<MultipartFile> fileList) throws NoUserException {
-        UserEntity userEntity = userRepository.findById(inquiryRequestDto.userId()).orElseThrow(NoUserException::new);
+    public InquiryResponseDto save(InquiryRequestDto inquiryRequestDto, List<MultipartFile> fileList) {
+        UserEntity userEntity = userRepository.findById(inquiryRequestDto.userId()).orElseThrow(() -> new CustomException(USER_NOT_FOUND, "해당 User를 찾을 수 없습니다."));
         Inquiry inquiry = inquiryRepository.save(inquiryRequestDto.toSaveInquiryEntity(userEntity));
 
         if(fileList != null && !fileList.isEmpty()){
@@ -78,8 +79,8 @@ public class InquiryService {
     }
 
     @Transactional
-    public Long update(InquiryUpdateDto inquiryUpdateDto, List<MultipartFile> fileList) throws  NoInquiryIdException{
-        Inquiry originInquiry = inquiryRepository.findById(inquiryUpdateDto.id()).orElseThrow(NoInquiryIdException::new);
+    public Long update(InquiryUpdateDto inquiryUpdateDto, List<MultipartFile> fileList) {
+        Inquiry originInquiry = inquiryRepository.findById(inquiryUpdateDto.id()).orElseThrow(() -> new CustomException(REQUIRED_ERROR, "문의 하신 게시글이 없습니다."));
         originInquiry.updateInquiry(inquiryUpdateDto);
         fileService.deleteByFileId(inquiryUpdateDto.deleteFileList());
 
@@ -91,8 +92,8 @@ public class InquiryService {
     }
 
     @Transactional
-    public void delete(Long id) throws NoInquiryIdException{
-        Inquiry inquiry = inquiryRepository.findById(id).orElseThrow(NoInquiryIdException::new);
+    public void delete(Long id) {
+        Inquiry inquiry = inquiryRepository.findById(id).orElseThrow(() -> new CustomException(REQUIRED_ERROR, "문의 하신 게시글이 없습니다."));
         inquiryRepository.deleteById(inquiry.getId());
 
         FilesLocation filesLocation = fileService.findByFileLocationId(1L);
@@ -104,8 +105,8 @@ public class InquiryService {
     }
 
     @Transactional
-    public Long answerSave(InquiryAnswerDto inquiryAnswerDto) throws NoInquiryIdException{
-        Inquiry inquiry = inquiryRepository.findById(inquiryAnswerDto.inquiryId()).orElseThrow(NoInquiryIdException::new);
+    public Long answerSave(InquiryAnswerDto inquiryAnswerDto) {
+        Inquiry inquiry = inquiryRepository.findById(inquiryAnswerDto.inquiryId()).orElseThrow(() -> new CustomException(REQUIRED_ERROR, "문의 하신 게시글이 없습니다."));
         InquiryAnswer inquiryAnswer = inquiryAnswerRepository.save(inquiryAnswerDto.toSaveInquiryAnswerEntity(inquiry));
         isCompleteInquiryUpdate(inquiry.getId(), true);
 
@@ -113,11 +114,11 @@ public class InquiryService {
     }
 
     @Transactional
-    public Long answerDelete(Long id, Long inquiryId) throws NoInquiryAnswerIdException, NoInquiryIdException{
-        InquiryAnswer inquiryAnswer = inquiryAnswerRepository.findById(id).orElseThrow(NoInquiryAnswerIdException::new);
+    public Long answerDelete(Long id, Long inquiryId) {
+        InquiryAnswer inquiryAnswer = inquiryAnswerRepository.findById(id).orElseThrow(() -> new CustomException(REQUIRED_ERROR, "문의 하신 게시글의 답변이 없습니다."));
         inquiryAnswerRepository.deleteById(id);
 
-        Inquiry originInquiry = inquiryRepository.findById(inquiryId).orElseThrow(NoInquiryIdException::new);
+        Inquiry originInquiry = inquiryRepository.findById(inquiryId).orElseThrow(() -> new CustomException(REQUIRED_ERROR, "문의 하신 게시글이 없습니다."));
         if(originInquiry.getInquiryAnswer().size() == 1){
             isCompleteInquiryUpdate(inquiryId, false);
         }
@@ -126,8 +127,8 @@ public class InquiryService {
     }
 
     @Transactional
-    public void isCompleteInquiryUpdate(Long id, Boolean isComplete) throws NoInquiryIdException{
-        Inquiry originInquiry = inquiryRepository.findById(id).orElseThrow(NoInquiryIdException::new);
+    public void isCompleteInquiryUpdate(Long id, Boolean isComplete) {
+        Inquiry originInquiry = inquiryRepository.findById(id).orElseThrow(() -> new CustomException(REQUIRED_ERROR, "문의 하신 게시글이 없습니다."));
         originInquiry.isCompleteInquiryUpdate(isComplete);
     }
 
