418c1bb06bea303fa88465819efbd9254a518ef3
seokyHan <sseok1979@naver.com>
2023-12-29 17:12:01 +0900

    add: houseDealParams 추가

--- src/main/java/com/todaysroom/map/types/HouseDealParams.java
+++ src/main/java/com/todaysroom/map/types/HouseDealParams.java
@@ -10,11 +10,7 @@ public enum HouseDealParams {
     PAGE_NO("pageNo"),
     NUM_OF_ROWS("numOfRows"),
     LAWD_CD("LAWD_CD"),
-    DEAL_YMD("DEAL_YMD"),
-    LOCAL_OF_AGENCY("중개사소재지"),
-    LEGAL_BUILDING("법정동"),
-    ROAD_NAME("도로명"),
-    ROAD_NAME_BUILDING_CODE("도로명건물본번호코드");
+    DEAL_YMD("DEAL_YMD");
 
     private final String key;
 
