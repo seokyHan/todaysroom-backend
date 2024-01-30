15f3eda4d7c592e5abe728395334cc659b718034
seokyHan <sseok1979@naver.com>
2024-01-30 18:48:50 +0900

    add: webClient dependency 추가

--- src/main/java/com/todaysroom/map/dto/HouseInfoDto.java
+++ src/main/java/com/todaysroom/map/dto/HouseInfoDto.java
@@ -2,7 +2,7 @@ package com.todaysroom.map.dto;
 
 public record CoordinatesDto(String x, String y) {
 
-    public static CoordinatesDto of(String x, String y){
+    public CoordinatesDto of(String x, String y){
         return new CoordinatesDto(x, y);
     }
 }
