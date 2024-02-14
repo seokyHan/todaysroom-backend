7f9f89fa57b86bf7309fe10c29e14f0c9a8ecb86
seokyHan <sseok1979@naver.com>
2024-02-14 17:58:35 +0900

    fix: refactoring Mono dto

--- src/main/java/com/todaysroom/map/controller/HouseMapController.java
+++ src/main/java/com/todaysroom/map/controller/HouseMapController.java
@@ -1,5 +1,7 @@
 package com.todaysroom.map.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.todaysroom.map.dto.HouseInfoDto;
 import com.todaysroom.map.service.HouseMapService;
 import lombok.RequiredArgsConstructor;
 import org.json.JSONArray;
@@ -7,9 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +23,7 @@ public class HouseMapController {
     private final HouseMapService houseMapService;
 
     @GetMapping("/getHouseInfo")
-    public ResponseEntity<String> houseInfo() {
+    public ResponseEntity<Mono<List<HouseInfoDto>>> houseInfo() {
 
         return ResponseEntity.ok(houseMapService.getHouseInfo());
     }
