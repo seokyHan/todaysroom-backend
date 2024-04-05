898dd43fc7dee2e47aceeee57e7240815e38541c
seokyHan <sseok1979@naver.com>
2024-04-05 18:02:48 +0900

    fix: exclude path 생성자 추가

--- src/main/java/com/todaysroom/global/security/config/JwtSecurityConfig.java
+++ src/main/java/com/todaysroom/global/security/config/JwtSecurityConfig.java
@@ -2,6 +2,7 @@ package com.todaysroom.global.security.config;
 
 import com.todaysroom.global.security.filter.JwtFilter;
 import com.todaysroom.global.security.jwt.TokenProvider;
+import com.todaysroom.global.security.props.ExcludeProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,10 +13,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     private final TokenProvider tokenProvider;
+    private final ExcludeProperties excludeProperties;
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        JwtFilter customFilter = new JwtFilter(tokenProvider);
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider, excludeProperties);
         http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
