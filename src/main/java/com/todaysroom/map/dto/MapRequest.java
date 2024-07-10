7a7f9fc824233e2a29eb7d771d6b39fd8abcf426
seokyHan <sseok1979@naver.com>
2024-07-10 18:22:07 +0900

    fix: dto 삭제

--- src/main/java/com/todaysroom/map/dto/MapRequest.java
+++
@@ -1,9 +0,0 @@
-package com.todaysroom.map.dto;
-
-import static org.apache.commons.lang3.StringUtils.split;
-
-public record MapRequest(String sidoName,
-                         String gugunName,
-                         String dongName) {
-
-}
